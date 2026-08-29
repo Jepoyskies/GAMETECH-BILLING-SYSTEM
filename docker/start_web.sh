417d120812280e8b36424444120ce8eef6fdc4ad
Jepoyskies <albertojosephernestp.2003@gmail.com>
2026-08-29 08:44:15 +0800

    Production cleanup: removing legacy files and optimizing Docker for Gunicorn/Whitenoise

--- docker/start_web.sh
+++ docker/start_web.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-
-
 echo "Starting Web server..."
 
+python manage.py collectstatic --noinput
 python manage.py migrate
 
-python manage.py runserver 0.0.0.0:8000
+exec gunicorn gametech_core.wsgi:application --bind 0.0.0.0:8000
--- gametech_core/settings.py
+++ gametech_core/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&#1lotwp@bi@r%8e25pr-41x=pux%otc3&u9jxx!r5=!i0y^aq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['http://143.198.207.144', 'https://143.198.207.144']
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
