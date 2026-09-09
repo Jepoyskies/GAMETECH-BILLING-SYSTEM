3de566562b4fc30f07e5e6b32cde8e597e4674c7
Jepoyskies <albertojosephernestp.2003@gmail.com>
2026-09-09 10:27:57 +0800

    Fix NameError: reverse is not defined in pay_customer_view

--- billing/views/payments.py
+++ billing/views/payments.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 from django.http import JsonResponse, FileResponse, HttpResponse
 import os
 from django.conf import settings
