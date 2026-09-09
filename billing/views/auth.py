41249bf1f75ae79a76a39594e3f336eb4a5edff4
Joseph Alberto <albertojosephernestp.2003@gmail.com>
2026-09-09 23:19:12 +0800

    Use custom logout view to enforce redirect and handle GET

--- billing/views/auth.py
+++ billing/views/auth.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.core.cache import cache
 from django.contrib.auth.decorators import login_required, user_passes_test, permission_required
 from django.views.decorators.http import require_POST
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from billing.decorators import role_required
 from django.contrib import messages
 from django.utils import timezone
@@ -345,3 +345,7 @@ def online_staff_api(request):
     return JsonResponse({'status': 'success', 'data': data})
 
 
+
+def custom_logout_view(request):
+    logout(request)
+    return redirect('login')
